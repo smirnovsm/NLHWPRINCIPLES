5eb3799d42cecf2dcea77f53149c807581b3f0dd
Sergei Smirnov <smirnov.sm91@yandex.ru>
2023-07-30 13:49:28 +0300

    Initial Commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday; // поле для дня рождения
+    public String name; // поле для имени
+    public String passport; // поле для номера пасспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для подписки
+
+}
